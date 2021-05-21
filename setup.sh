dbc1474e50fc895fc15544abdd1b9f29a1f95985
Tobin Stultiens <tobin.stultiens@gmail.com>
2021-05-22 00:50:43 +0200

    Created a setup

--- setup.sh
+++ setup.sh
@@ -1,2 +1,15 @@
 # Add current user to the docker group
 sudo usermod -a -G docker $USER
+
+# Install docker-compose
+sudo apt install docker-compose
+
+# Install sshfs
+sudo apt install sshfs
+
+# Install curl
+sudo apt install curl
+
+# Install nvidia-docker2
+sudo apt-get update
+sudo apt-get install -y nvidia-docker2
