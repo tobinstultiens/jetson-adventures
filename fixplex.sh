ff8b91a94088011e6e5b382f31cb224270ac3e09
Mario Lucassen <Mario.lucassen@hotmail.com>
2021-05-22 17:17:47 +0200

    Added script to set envs for fix plex

--- fixplex.sh
+++ fixplex.sh
@@ -1,5 +1,14 @@
-rmdir /mnt/NAS/$LOCATION/Download
-rmdir /mnt/NAS/$LOCATION/Books
-rmdir /mnt/NAS/$LOCATION
-sshfs -o allow_other $USER@$IP:/ /mnt/NAS
+#!/bin/sh
+# cleans up mount info
+sudo umount /mnt/PLEX
+
+sudo rmdir --ignore-fail-on-non-empty /mnt/PLEX/Download
+sudo rmdir --ignore-fail-on-non-empty /mnt/PLEX/Books
+
+. ./setEnvs.sh
+
+# mounts endpoint
+sudo sshfs -o allow_other,IdentityFile=$SSH_KEY $MOUNT_USER@$MOUNT_ENDPOINT:$MOUNT_LOCATION /mnt/PLEX
+
+# restart docker compose
 docker-compose restart plex
---
+++ setEnvs.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+export MOUNT_LOCATION=/mnt/pve/ree/media
+export MOUNT_USER=plexer
+export MOUNT_ENDPOINT=proxmox 
+export SSH_KEY=/home/$USER/.ssh/id_rsa
